69d6ba48b796c5cd917df9fe54290900fec7310c
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-01-24 22:19:32 +0700

    Add project files.

---
+++ ChatMVCsignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatMVCsignalR.Startup))]
+namespace ChatMVCsignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
